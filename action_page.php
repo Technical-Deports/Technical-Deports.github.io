0cc6635dc1c4a825b27731948cd47aa0d208a0ab
EdwinAlvarado10 <99769731+EdwinAlvarado10@users.noreply.github.com>
2022-03-22 17:05:45 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,7 +3,7 @@
     Bienvenido <?php echo $_POST["Nombres"]; ?><br>
    <?php echo $_POST["lname"]; ?>
     Tu Correo Electronico es: <?php echo $_POST["Correo Electronico"]; ?><br>
-   Tu Número Telefonico es: <?php echo $_POST["Número Telefonico"]; ?><br>
+   Tu Número Telefonico es: <?php echo $_POST["Número Telefonico"]; ?>
     </body>
     </html>
     
