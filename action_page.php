fe9d55e59e6a2fdc48b863c7ca4e1d27a338195d
EdwinAlvarado10 <99769731+EdwinAlvarado10@users.noreply.github.com>
2022-03-22 16:49:56 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,3 +1,7 @@
 <html>
   <body>
-    Bienvenido <?php echo $_POST["name]; ?
+    Bienvenido <?php echo $_POST["name"]; ?><br>
+    Tu Correo Electronico es: <?php echo $_POST["email"]; ?>
+    </body>
+    </html>
+    
